4543cf4bd8b309cacf4545127a51fc7c06b69b93
Elise_LaBonte <emlabonte714@gmail.com>
2021-09-29 20:14:38 -0400

    added category to index.js exports

--- controllers/api/categoryRoutes.js
+++ controllers/api/categoryRoutes.js
@@ -2,11 +2,12 @@ const router = require('express').Router();
 const { Category, Post } = require('../../models');
 // why isn't Category being imported as a class?
 
+
 // The `/api/categories` endpoint
 
 router.get('/', async (req, res) => {
   // find all categories
-  // be sure to include its associated Post data
+  // be sure to include the associated Post data
     try{
         const categories = await Category.findAll({
         include: [{model: Post}]
@@ -20,12 +21,12 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-  // find a single tag by its `id`
+  // find a single category by its `id`
     try {
-        const categories = await Category.findByPk(req.params.id, {
+        const category = await Category.findByPk(req.params.id, {
         include:  [{model: Post}]
         })
-        res.json(categories)
+        res.json(category)
     }
     catch{
         res.status(404).json(err)
@@ -33,7 +34,7 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  // create a new tag
+  // create a new category
     try {
         const newCategory = await Category.create(req.body)
         res.status(200).json(newCategory)
@@ -42,15 +43,15 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
-    Tag.update({
+  // update a category's name by its `id` value
+    Category.update({
         where: { id: req.params.id }, 
     })
         .catch((err) => res.status(500).json(err))
 });
 
 router.delete('/:id', async (req, res) => {
-  // delete on tag by its `id` value
+  // delete on category by its `id` value
     try {
         const category = await Category.destroy({
         where: {id: req.params.id}
