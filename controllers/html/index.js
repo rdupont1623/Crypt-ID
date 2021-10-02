2a833c0eac4df4269a5943d58067105643c8ff14
Elise_LaBonte <emlabonte714@gmail.com>
2021-10-02 14:16:32 -0400

    working again

--- controllers/html/index.js
+++ controllers/html/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const homeRoutes = require('./homeRoutes');
-const authRoutes = require('./authRouter');
+const authRoutes = require('./authRoutes');
 const postRoutes = require('./postRoutes');
 
 router.use('/', homeRoutes);
