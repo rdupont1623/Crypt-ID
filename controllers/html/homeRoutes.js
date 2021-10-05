8d5e79b13e249bd6483a5f76dfffbc3a6e6f3649
Stevanie Kanter <stevaniek@yahoo.com>
2021-10-04 20:55:24 -0400

    fixed login

--- controllers/html/homeRoutes.js
+++ controllers/html/homeRoutes.js
@@ -65,7 +65,8 @@ router.get('/profile', withAuth, async (req, res) => {
 
     res.render('profile', {
       ...user,
-      logged_in: true
+      logged_in: true,
+      posts:[]
     });
   } catch (err) {
     res.status(500).json(err);
