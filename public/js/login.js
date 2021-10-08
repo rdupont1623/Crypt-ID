15cfdc06dcec33488711a013aab7d3de3046d9d1
Nick Zabkar <nzabkar51@gmail.com>
2021-10-08 10:45:31 -0400

    Changed redirect after login to homepage

--- public/js/login.js
+++ public/js/login.js
@@ -15,7 +15,7 @@ const loginFormHandler = async (event) => {
 
     if (response.ok) {
       // If successful, redirect the browser to the create page
-      document.location.replace('/create');
+      document.location.replace('/');
     } else {
       alert(response.statusText);
     }
