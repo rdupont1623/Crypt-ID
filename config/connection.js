9739e4d8c4266691a53286ff332f6eec69177793
Elise_LaBonte <emlabonte714@gmail.com>
2021-10-13 21:47:19 -0400

    connection for AWS

--- config/connection.js
+++ config/connection.js
@@ -5,18 +5,17 @@ const environment = process.env;
 let sequelize;
 
 if (environment.JAWSDB_URL) {
-  sequelize = new Sequelize(environment.JAWSDB_URL);
+    sequelize = new Sequelize(environment.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(
-    environment.DB_NAME,
-    environment.DB_USER,
-    environment.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
+    sequelize = new Sequelize(
+        environment.DB_NAME,
+        environment.DB_USER,
+        environment.DB_PASSWORD, {
+            host: DB_ENDPOINT,
+            dialect: 'mysql',
+            port: DB_PORT
+        }
+    );
 }
 
 module.exports = sequelize;
